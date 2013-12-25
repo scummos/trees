695ddb717f3d0d9615af3847bf6c528689ed5432
Sven Brauch <svenbrauch@googlemail.com>
2013-12-25 21:01:45 +0100

    working good, except that it crashes on exit

--- trees.py
+++ trees.py
@@ -8,9 +8,12 @@ from IPython.core.display import Image as ImageDisplay
 import random
 from PyQt4.QtGui import QImage, QPainter, QColor, QPolygon
 from PyQt4 import Qt
-from PyQt4.QtCore import QPoint, QObject
+from PyQt4.QtCore import QPoint, QObject, QPointF, QLineF
 
 from PyQt4.QtGui import QLabel, QApplication, QPixmap, QMainWindow, QPushButton, QVBoxLayout, QWidget, QDialog
+from PyQt4.QtGui import QPen, QGraphicsScene, QPolygonF
+
+image_shape = (1200, 800)
 
 class Box:
     def __init__(self, xmin, xmax, ymin, ymax):
@@ -24,6 +27,13 @@ class Box:
         self.xmax = self.xmax if self.xmax > other.xmax else other.xmax
         self.ymin = self.ymin if self.ymin < other.ymin else other.ymin
         self.ymax = self.ymax if self.ymax > other.ymax else other.ymax
+
+    def enlarge(self, by_percent):
+        f = by_percent / 100.0 / 2.0
+        self.xmin -= self.size()[0] * f
+        self.xmax += self.size()[0] * f
+        self.ymin -= self.size()[1] * f
+        self.ymax += self.size()[1] * f
     
     def fix_aspect_ratio(self, target_ratio):
         target = target_ratio[0] / target_ratio[1]
@@ -54,9 +64,11 @@ class Branch:
         self.branch_after = random.randint(*self.params["branch_after_range"])
         self.is_alive = True
         self.is_dying = False
-        self.gravity = 0.025
+        self.gravity = self.params["gravity"]
         self.generation = generation
         self.already_drawn = None
+        self.down_damping_x = self.params["down_damping_x"]
+        self.down_damping_y = self.params["down_damping_y"]
     
     def apply_gravity(self):
         weight = np.sin(np.arctan2(np.real(self.velocity), np.imag(self.velocity)))
@@ -64,8 +76,9 @@ class Branch:
         pointing_down = np.imag(self.velocity) < 0
         self.velocity -= complex(0, accel) * (1 if not pointing_down else 3)
         if pointing_down:
-            self.velocity = complex(np.real(self.velocity) * 0.94, np.imag(self.velocity) * 0.97)
-            if random.randrange(1, 25) == 4:
+            self.velocity = complex(np.real(self.velocity) * self.down_damping_x,
+                                    np.imag(self.velocity) * self.down_damping_y)
+            if random.randrange(1, self.params["down_die_probability"]) == 4:
                 self.is_dying = True
     
     def die_if_too_old(self):
@@ -90,23 +103,36 @@ class Branch:
         self.history.append(self.position)
 
         if len(self.history) > self.branch_after:
-            self.is_alive = False
+            if self.params["keep_central"] < random.uniform(0, 1):
+                self.is_alive = False
             if self.is_dying:
                 return
-            v1 = self.velocity - self.params["branch_split"]*(1 + self.params["branch_split_var"] * random.random()-0.5)
-            v2 = self.velocity + self.params["branch_split"]*(1 + self.params["branch_split_var"] * random.random()-0.5)
-            self.branches.append(Branch(self.position, v1, self.params, self.generation + 1))
-            self.branches.append(Branch(self.position, v2, self.params, self.generation + 1))
+            self.do_branch()
 
-    def draw_into(self, painter, coordinate_transform, incremental=False):
+    def do_branch(self, count=2):
+        v1 = self.velocity - self.params["branch_split"]*(1 + self.params["branch_split_var"] * random.random()-0.5)
+        v2 = self.velocity + self.params["branch_split"]*(1 + self.params["branch_split_var"] * random.random()-0.5)
+        self.branches.append(Branch(self.position, v1, self.params, self.generation + 1))
+        self.branches.append(Branch(self.position, v2, self.params, self.generation + 1))
+
+    def draw_into(self, scene, incremental=False):
         for subbranch in self.branches:
-            subbranch.draw_into(painter, coordinate_transform, incremental)
-        previous = None
+            subbranch.draw_into(scene, incremental)
         start = self.already_drawn if incremental else None
-        points = QPolygon([QPoint(*x) for x in map(coordinate_transform, self.history[start:])])
+        points = [QPointF(np.real(x), -np.imag(x)) for x in self.history[start:]]
+
+        gens = float(self.params["painter_generations"])
+        pen_width = max(0, gens-self.generation) / gens * self.params["painter_thickness"]
+        color = self.params["color"]
+        assert isinstance(color, QColor)
+        color.setAlpha(17 + 99 * max(0, gens-self.generation) / gens)
+        pen = QPen(color)
+        pen.setWidthF(pen_width)
+        for index in range(len(points) - 1):
+            scene.addLine(QLineF(points[index], points[index+1]), pen)
+
         if incremental:
-            self.already_drawn = len(self.history)
-        painter.drawPolyline(points)
+            self.already_drawn = max(0, len(self.history) - 1)
 
     def get_bounding_box(self):
         if len(self.branches) == 0:
@@ -122,13 +148,13 @@ class Branch:
         for box in children_bounds:
             own_bounds.grow_to(box)
         return own_bounds
-            
+
 
 class Coordinates:
     def __init__(self, bounding_box, resolution):
         self.bounding_box = bounding_box
         self.resolution = resolution
-    
+
     def __call__(self, *args, **kwargs):
         source_coords = args[0]
         res = (
@@ -139,58 +165,82 @@ class Coordinates:
 
 class Tree:
     def __init__(self, params):
-        self.trunk = Branch(10, 2.0j, params)
-    
+        self.trunk = Branch(10, params["v_start"], params)
+        start = params["start_branches"]
+        if start > 1:
+            self.trunk.is_alive = False
+            for i in range(start / 2):
+                self.trunk.do_branch()
+            if start % 2 == 1:
+                self.trunk.branches[0].is_alive = False
+                self.trunk.do_branch()
+
     def grow_iterations(self, steps=20, yield_every=0):
         for iteration in range(steps):
             self.trunk.grow()
             if yield_every != 0 and iteration % yield_every == 0:
                 yield
-    
-    def draw(self, existing_image=None, incremental=False, bounding_box=None):
-        only_incremental = False
-        shape = (400, 400)
-        image = QImage(shape[0], shape[1], QImage.Format_RGB32) if existing_image is None else existing_image
-        if existing_image is None and not only_incremental:
-            image.fill(0)
-        if bounding_box is None:
-            bounds = self.trunk.get_bounding_box()
-            bounds.fix_aspect_ratio(shape)
-        else:
-            bounds = bounding_box
-        coords = Coordinates(bounds, shape)
-        painter = QPainter(image)
-        painter.setRenderHints(QPainter.HighQualityAntialiasing)
-        painter.setPen(QColor(255, 255, 255, 60))
-        self.trunk.draw_into(painter, coords, incremental)
-        return image
 
-def new_anim():
-    image = None
-    t = Tree(params = {
-            "branch_split": 0.35,
-            "branch_after_range": (12, 32),
-            "branch_split_var": 1.4,
-        })
-    for iteration in t.grow_iterations(250, yield_every=5):
-        image = t.draw(image, incremental=True, bounding_box=Box(-200, 260, 0, 460))
-        pixmap = QPixmap.fromImage(image)
-        widget.setPixmap(pixmap)
-        widget.repaint()
-        QApplication.processEvents()
+    def draw(self, scene, incremental=False):
+        self.trunk.draw_into(scene, incremental)
+
+class TreeDialog(QDialog):
+    def __init__(self):
+        import treedialog
+        QDialog.__init__(self)
+        self.ui = treedialog.Ui_Dialog()
+        self.ui.setupUi(self)
+        self.ui.draw_buton.clicked.connect(self.new_anim)
+        self.scene = QGraphicsScene()
+        self.ui.image.setScene(self.scene)
+        self.ui.image.setRenderHints(QPainter.HighQualityAntialiasing)
+
+    def get_params(self):
+        params = dict()
+        params["branch_split"] = self.ui.branch_split.value()
+        params["branch_after_range"] = (self.ui.branch_after_min.value(), self.ui.branch_after_max.value())
+        params["branch_split_var"] = self.ui.branch_split_var.value()
+        params["gravity"] = self.ui.gravity.value()
+        params["down_die_probability"] = self.ui.down_die_probability.value()
+        params["down_damping_x"] = self.ui.down_damping_x.value()
+        params["down_damping_y"] = self.ui.down_damping_x.value()
+        params["start_branches"] = self.ui.start_branches.value()
+        params["keep_central"] = self.ui.keep_central.value()
+        params["color"] = QColor(self.ui.r.value() * 255, self.ui.g.value() * 255, self.ui.b.value() * 255)
+        params["color_speed"] = self.ui.color_speed.value()
+        params["painter_thickness"] = self.ui.painter_thickness.value()
+        params["painter_generations"] = self.ui.painter_generations.value()
+        start_rand = [random.uniform(-1, 1) * self.ui.v_start_var.value() for i in range(2)]
+        params["v_start"] = complex(self.ui.v_start_x.value()+start_rand[0], self.ui.v_start_y.value()+start_rand[1])
+        return params
+
+    def new_anim(self):
+        image = None
+        painter_id = random.randint(0, 2**32)
+        self.active_painer = painter_id
+        self.tree = Tree(self.get_params())
+        index = 0
+        every = self.ui.repaint.value()
+
+        self.scene.setBackgroundBrush(QColor(0, 0, 0))
+        self.scene.clear()
+        for iteration in self.tree.grow_iterations(self.ui.generations.value(), yield_every=every):
+            self.tree.draw(self.scene, incremental=True)
+            self.ui.progress.setText("Working ... displayed frame: {0}".format(index))
+            self.ui.image.repaint()
+            QApplication.processEvents()
+            index += every
+            if self.active_painer != painter_id:
+                return
+        self.ui.progress.setText("Done. Displayed frame: {0}".format(index))
 
 if __name__ == '__main__':
     import sys
     app = QApplication(sys.argv)
-    window = QDialog()
+    window = TreeDialog()
     widget = QLabel()
-    button = QPushButton("another one!")
-    window.setLayout(QVBoxLayout())
-    window.layout().addWidget(button)
-    window.layout().addWidget(widget)
-    button.clicked.connect(new_anim)
     window.show()
-    new_anim()
+    window.new_anim()
     app.exec_()
 
 
